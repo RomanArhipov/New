898664e734b76b34c8a3257c1fbb2b1d27b6b5c8
RomanArhipov <35568026+RomanArhipov@users.noreply.github.com>
2020-01-08 23:04:50 +0300

    Add files via upload

---
+++ Shablon/DZ2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AppleStore appleStore = new AppleStore();
+        Samsung samsung = new Samsung();
+
+        appleStore.Catalog();
+        samsung.Catalog();
+
+        Console.Read();
+    }
+}
+abstract class Shop
+{
+    public void Catalog()
+    {
+        Phones();
+        TV();
+        Sales();
+        Pad();
+    }
+    public abstract void Phones();
+    public abstract void TV();
+    public virtual void Sales()
+    {
+        Console.WriteLine("Акция!!! 2 по цене 1!!!");
+        Console.WriteLine("\n");
+    }
+    public abstract void Pad();
+}
+
+class AppleStore : Shop
+{
+    public override void Pad()
+    {
+        Console.WriteLine("Catalog of iPad:\n");
+
+        Console.WriteLine("iPad 1");
+        Console.WriteLine("iPad 2");
+        Console.WriteLine("iPad 3");
+        Console.WriteLine("\n");
+    }
+
+    public override void Phones()
+    {
+        Console.WriteLine("Catalog of iPhone:\n");
+
+        Console.WriteLine("iPhone X");
+        Console.WriteLine("iPhone XR");
+        Console.WriteLine("iPhone 11");
+        Console.WriteLine("\n");
+    }
+
+    public override void TV()
+    {
+        Console.WriteLine("Catalog of TV:\n");
+
+        Console.WriteLine("Apple TV");
+        Console.WriteLine("Apple TV2");
+        Console.WriteLine("\n");
+    }
+
+  
+}
+
+class Samsung : Shop
+{
+    public override void Phones()
+    {
+        Console.WriteLine("Catalog of Phone:\n");
+
+        Console.WriteLine("Samsung galaxy S7");
+        Console.WriteLine("Samsung galaxy S8");
+        Console.WriteLine("Samsung galaxy S9");
+        Console.WriteLine("\n");
+    }
+
+    public override void TV()
+    {
+        Console.WriteLine("Catalog of TV:\n");
+
+        Console.WriteLine("Samsung TV");
+        Console.WriteLine("Samsung TV 2");
+        Console.WriteLine("Samsung TV 3");
+        Console.WriteLine("\n");
+    }
+
+    public override void Pad()
+    {
+        Console.WriteLine("Catalog of Tab:\n");
+
+        Console.WriteLine("Samsung galaxy Tab");
+        Console.WriteLine("Samsung galaxy Tab 2");
+        Console.WriteLine("Samsung galaxy Tab 3");
+        Console.WriteLine("\n");
+    }
+}
---
+++ Strateg/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Strateg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Train locomotive = new Train(1000, "Siemens", new СoalMove());
+            locomotive.Move();
+            locomotive.Movable = new ElectricMove();
+            locomotive.Move();
+
+            Console.ReadLine();
+        }
+    }
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class СoalMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("\nПеремещение на углях");
+        }
+    }
+
+    class ElectricMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("\nПеремещение на электричестве");
+        }
+    }
+    class Train
+    {
+        protected int passengers; // кол-во пассажиров
+        protected string model; // модель поезда
+
+        public Train(int num, string model, IMovable mov)
+        {
+            this.passengers = num;
+            this.model = model;
+            Movable = mov;
+        }
+        public IMovable Movable { private get; set; }
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+}
